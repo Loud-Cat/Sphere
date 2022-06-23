6ef7e40442ba5cbde8fe94767951dfc1ae873255
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2022-06-22 20:44:06 -0500

    Update sketch.js

--- rings/sketch.js
+++ rings/sketch.js
@@ -1,7 +1,7 @@
-/* Approximating A Sphere
- * This demo uses many tiny spheres to roughly
- * approximate the shape of a sphere
-*/
+/* Approximating a sphere
+ * In this demo, we will use rings (tori) to
+ * roughly display a sphere
+ */
 
 // When this variable is true, the animation will
 // Stop after one finished animation to make it easier
@@ -15,28 +15,22 @@ function setup() {
 
 function draw() {
   background(225);
-  rotateX(frameCount / 100);
-  rotateY(frameCount / 100);
+  rotateX(frameCount / 150);
+  rotateY(frameCount / 150);
   normalMaterial();
 
-  let step = PI / 25;
   let radius = min(width, height) / 3;
-  for (let t = 0; t < PI + step; t += step) {
+  for (let t = 0; t < PI; t += PI / 25) {
     let offset = cos(t) * radius;
     let r = abs( sin(t) ) * radius;
 
-    for (let t2 = 0; t2 < TWO_PI; t2 += PI / 8) {
-      let x = r * cos(t2);
-      let y = r * sin(t2);
-      
-      push();
-      translate(x, y, offset);
-      sphere(5);
-      pop();
-    }
+    push();
+    translate(0, 0, offset);
+    torus(r, 2);
+    pop();
   }
   
   // stop looping after one full animation
-  if (stop_anim && frameCount / 100 >= TWO_PI)
+  if (stop_anim && frameCount / 150 >= TWO_PI)
     noLoop();
 }
